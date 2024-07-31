316ece17d735ba185efd09a60d8b80dba541fbb8
Ilya Kolesov <kolesovilya3@gmail.com>
2024-07-31 17:33:33 +0300

    first commit

---
+++ server/src/main/java/com/game/draw/DrawWithWordsApplication.java
@@ -0,0 +1,13 @@
+package com.game.draw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawWithWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawWithWordsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/game/draw/DrawWithWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.draw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawWithWordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
