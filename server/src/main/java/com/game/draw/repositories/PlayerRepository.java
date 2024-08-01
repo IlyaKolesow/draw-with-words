c2a00f26f1a18785b421758dc3ea533d4cb273b8
Ilya Kolesov <kolesovilya3@gmail.com>
2024-08-01 17:22:20 +0300

    feat: create repositories

---
+++ server/src/main/java/com/game/draw/repositories/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.game.draw.repositories;
+
+import com.game.draw.models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
+}
