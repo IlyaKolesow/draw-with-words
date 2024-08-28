904b5c73c1dc7519bcfcc3618303455aec27f831
Ilya Kolesov <kolesovilya3@gmail.com>
2024-08-28 14:42:09 +0300

    feat: create index page

---
+++ client/src/js/index.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById("auth-btn");
+
+btn.addEventListener("click", () => {
+
+    let playerName = document.getElementById("auth-input").value;
+
+    fetch("http://localhost:8080/players", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: playerName
+        })
+    })
+    .then(response => response.json())
+    .then(player => {
+        sessionStorage.setItem("playerName", player.name);
+        sessionStorage.setItem("playerId", player.player_id);
+    });
+
+    location = "rooms.html";
+});
